c0be77299daff91914be972386f824b71e97b829
John Hughes <john.hughes101@gmail.com>
2020-04-04 12:51:19 +0100

    add binding functionality to blog Entry struct

--- pkg/icon/blog/service.go
+++ pkg/icon/blog/service.go
@@ -1,5 +1,17 @@
 package blog
 
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/music-tribe/uuid"
+)
+
 // Entry -
 type Entry struct {
 	UUID      uuid.UUID  `json:"uuid,omitempty"`
@@ -13,22 +25,35 @@ type Entry struct {
 // BindRequestParams -
 func (e *Entry) BindRequestParams(r *http.Request) error {
 	routeParams := mux.Vars(r)
-	JSONBuffer, err := storeRouteParamsAsJSONInBuffer(routeParams)
+	JSONBuffer, err := encodeRouteParamsToJSONBuffer(routeParams)
 	if err != nil {
 		return err
 	}
 	return e.bindJSONBufferToBlogEntry(JSONBuffer)
 }
 
-func storeRouteParamsAsJSONInBuffer(routeParams map[string]string) (*bytes.Buffer, error) {
+// BindRequestBody -
+func (e *Entry) BindRequestBody(r *http.Request) error {
+	if r.Body == nil {
+		return errors.New("request body is empty")
+	}
+	defer r.Body.Close()
+	byt, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	return e.bindJSONBufferToBlogEntry(bytes.NewBuffer(byt))
+}
+
+func encodeRouteParamsToJSONBuffer(routeParams map[string]string) (*bytes.Buffer, error) {
 	JSONBuffer := bytes.NewBuffer([]byte{})
 	err := json.NewEncoder(JSONBuffer).Encode(routeParams)
 	if err != nil {
 		return nil, err
 	}
-	return JSONBuffer
+	return JSONBuffer, nil
 }
 
-func (e *Entry)bindJSONBufferToBlogEntry(JSONBuffer *bytes.Buffer) error {
+func (e *Entry) bindJSONBufferToBlogEntry(JSONBuffer *bytes.Buffer) error {
 	return json.NewDecoder(JSONBuffer).Decode(&e)
 }
