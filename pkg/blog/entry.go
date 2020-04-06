c70372db16d757d388fb8a3d3ff42bc796ceaddc
John Hughes <john.hughes101@gmail.com>
2020-04-06 20:58:46 +0100

    Changed Entry to data structuredue to db use

--- pkg/blog/entry.go
+++ pkg/blog/entry.go
@@ -1,7 +1,9 @@
 package blog
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -10,27 +12,32 @@ import (
 )
 
 // Entry shows the available methods on the Entry interface
-type Entry interface {
-	SetEntryFieldsFromRequestBody(r *http.Request) error
-	SetBlogEntryID(uuid.UUID)
-	ReturnEntryAsJSON() ([]byte, error)
-}
+// type Entry interface {
+// 	SetEntryFieldsFromRequestBody(r *http.Request) error
+// 	ReturnEntryAsJSON() ([]byte, error)
+// 	GetStoragePath() string
+// }
 
-type entry struct {
-	UUID      uuid.UUID  `json:"uuid,omitempty"`
-	Author    string     `json:"author,omitempty"`
-	Title     string     `json:"title,omitempty"`
-	Content   string     `json:"content,omitempty"`
-	CreatedAt *time.Time `json:"date,omitempty"`
-	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+// Entry holds all data and content from a blog post
+type Entry struct {
+	UUID        uuid.UUID  `json:"_id" bson:"id"`
+	Author      string     `json:"author"`
+	Title       string     `json:"title"`
+	CreatedAt   *time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at" bson:"updated_at"`
+	Content     string     `json:"content"`
+	Category    string     `json:"category"`
+	StoragePath string     `json:"storage_path"`
+	Images      []string   `json:"images"` // a list of image IDs - images already uploaded
 }
 
 // NewEntry creates a new
-func NewEntry() Entry {
-	return &entry{}
+func NewEntry() *Entry {
+	return &Entry{}
 }
 
-func (e *entry) SetEntryFieldsFromRequestBody(r *http.Request) error {
+// SetEntryFieldsFromRequestBody -
+func (e *Entry) SetEntryFieldsFromRequestBody(r *http.Request) error {
 	byt, err := ioutil.ReadAll(r.Body)
 	if r.Body != nil {
 		defer r.Body.Close()
@@ -38,74 +45,38 @@ func (e *entry) SetEntryFieldsFromRequestBody(r *http.Request) error {
 	if err != nil {
 		return err
 	}
-	return json.Unmarshal(byt, &e)
+	if err = e.bindJSONDataToBlogEntry(byt); err != nil {
+		return err
+	}
+	e.setUniqueIDForBlogEntry()
+	e.setStoragePath()
+	return nil
 }
 
-func (e *entry) SetBlogEntryID(id uuid.UUID) {
-	e.UUID = id
+func (e *Entry) setStoragePath() {
+	e.StoragePath = fmt.Sprintf(
+		"/%v/%v/%v/%s",
+		e.CreatedAt.Year(),
+		e.CreatedAt.Month(),
+		e.CreatedAt.Day(),
+		e.Title,
+	)
 }
 
-func (e *entry) ReturnEntryAsJSON() ([]byte, error) {
+func (e *Entry) setUniqueIDForBlogEntry() {
+	e.UUID = uuid.New()
+}
+
+// ReturnEntryAsJSON -
+func (e *Entry) ReturnEntryAsJSON() ([]byte, error) {
 	return json.Marshal(e)
 }
-
-// // BindRouteParamsQueriesAndBody
-// func (e *entry) BindRouteParamsQueriesAndBody(r *http.Request) error {
-// 	if err := e.BindRequestParams(r); err != nil {
-// 		return err
-// 	}
-// 	if err := e.BindRequestQueries(r); err != nil {
-// 		return err
-// 	}
-// 	return e.BindRequestBody(r)
-// }
-
-// // BindRequestParams -
-// func (e *entry) BindRequestParams(r *http.Request) error {
-// 	routeParams := mux.Vars(r)
-// 	JSONBuffer, err := encodeRouteParamsToJSONBuffer(routeParams)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return e.bindJSONBufferToBlogEntry(JSONBuffer)
-// }
-
-// // BindRequestBody -
-// func (e *entry) BindRequestBody(r *http.Request) error {
-// 	if r.Body == nil {
-// 		return errors.New("request body is empty")
-// 	}
-// 	defer r.Body.Close()
-// 	byt, err := ioutil.ReadAll(r.Body)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return e.bindJSONBufferToBlogEntry(bytes.NewBuffer(byt))
-// }
-
-// // BindRequestQueries -
-// func (e *entry) BindRequestQueries(r *http.Request) error {
-// 	queriesReadyForBinding := flattenQueryMapValues(r.URL.Query())
-// 	JSONBuffer, err := encodeRouteParamsToJSONBuffer(queriesReadyForBinding)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return e.bindJSONBufferToBlogEntry(JSONBuffer)
-// }
-
-// func flattenQueryMapValues(queryMap map[string][]string) map[string]string {
-// 	flatQueryMap := make(map[string]string)
-// 	for key, value := range queryMap {
-// 		flatQueryMap[key] = value[0]
-// 	}
-// 	return flatQueryMap
-// }
 
 // func encodeRouteParamsToJSONBuffer(routeParams map[string]string) (*bytes.Buffer, error) {
 // 	JSONBuffer := bytes.NewBuffer([]byte{})
 // 	return JSONBuffer, json.NewEncoder(JSONBuffer).Encode(routeParams)
 // }
 
-// func (e *entry) bindJSONBufferToBlogEntry(JSONBuffer *bytes.Buffer) error {
-// 	return json.NewDecoder(JSONBuffer).Decode(&e)
-// }
+func (e *Entry) bindJSONDataToBlogEntry(JSONBytes []byte) error {
+	return json.NewDecoder(bytes.NewBuffer(JSONBytes)).Decode(&e)
+}
