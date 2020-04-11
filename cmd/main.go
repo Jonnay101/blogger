72986c0a97fa71ef3947d95dbc22bfd84e2d42d3
John Hughes <john.hughes101@gmail.com>
2020-04-12 00:52:34 +0100

    configured custom server

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Jonnay101/icon/pkg/blog"
 	"github.com/Jonnay101/icon/pkg/database"
@@ -10,7 +12,6 @@ import (
 )
 
 func main() {
-	// all packages will be initialized in here
 	if err := Run(); err != nil {
 		log.Error(err.Error())
 		os.Exit(1)
@@ -19,9 +20,30 @@ func main() {
 
 // Run -
 func Run() error {
-	database := database.NewDatabaseSession()
+	database, err := database.NewDatabaseSession()
+	if err != nil {
+		return err
+	}
 	blog := blog.NewServer()
 	blog.SetDatabase(database)
-	log.Fatal(http.ListenAndServe(":8080", blog))
+	srv := configureServer(getPortFromEnvVars(), blog)
+	log.Fatal(srv.ListenAndServe())
 	return nil
+}
+
+func configureServer(port string, handler http.Handler) http.Server {
+	return http.Server{
+		Addr:         fmt.Sprintf(":%s", port),
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		Handler:      handler,
+	}
+}
+
+func getPortFromEnvVars() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	return port
 }
