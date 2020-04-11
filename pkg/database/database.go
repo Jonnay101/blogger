d6a206fb3a22a85ad2370d2d3b22eb352b2b10d5
John Hughes <john.hughes101@gmail.com>
2020-04-11 19:19:57 +0100

    initialize db

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,1 +1,17 @@
 package database
+
+import "github.com/Jonnay101/icon/pkg/blog"
+
+// Session -
+type Session struct{}
+
+// NewDatabaseSession -
+func NewDatabaseSession() *Session {
+	return &Session{}
+}
+
+// CreateBlogPost -
+func (s *Session) CreateBlogPost(blog.PostData) error {
+	// create a new blog post in the database
+	return nil
+}
