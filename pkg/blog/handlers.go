c7d0da700ffd7b60eda2b1867d8d22e94cd5ce2d
John Hughes <john.hughes101@gmail.com>
2020-04-12 11:58:00 +0100

    singular responsibility in handlers

--- pkg/blog/handlers.go
+++ pkg/blog/handlers.go
@@ -1,27 +1,73 @@
 package blog
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/Jonnay101/icon/pkg/glitch"
+	"github.com/music-tribe/uuid"
 )
 
 // HandlerCreatePost stores a newly created blog post
 func (s *server) HandlerCreatePost() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		var blogPost PostData
-		if err := s.decodeRequestBody(w, r, &blogPost); err != nil {
-			s.respond(w, r, err, http.StatusBadRequest)
-			return
-		}
+		blogPost := s.bindRequestBody(w, r)
 
-		blogPost.setUUID()
-		blogPost.setDateCreatedAt()
-		blogPost.setDatabaseKey()
+		s.setBlogPostFields(w, r, blogPost)
 
-		if err := s.DB.CreateBlogPost(&blogPost); err != nil {
-			s.respond(w, r, err, http.StatusInternalServerError)
-		}
+		s.storeBlogPost(w, r, blogPost)
 
 		s.respond(w, r, blogPost, http.StatusOK)
+		return
+	}
+}
+
+func (s *server) bindRequestBody(w http.ResponseWriter, r *http.Request) *PostData {
+
+	var blogPost PostData
+	if err := s.decodeRequestBody(w, r, &blogPost); err != nil {
+		s.respond(w, r, err, http.StatusBadRequest)
+		return nil
+	}
+	return &blogPost
+}
+
+func (s *server) setBlogPostFields(w http.ResponseWriter, r *http.Request, blogPost *PostData) {
+
+	blogPost.UUID = uuid.New()
+
+	currentTime := time.Now()
+	blogPost.CreatedAt = currentTime
+	blogPost.UpdatedAt = currentTime
+
+	blogPost.DatabaseKey = s.createDatabaseKey(w, r, blogPost)
+}
+
+func (s *server) createDatabaseKey(w http.ResponseWriter, r *http.Request, pd *PostData) string {
+	if pd.CreatedAt.IsZero() {
+		s.respond(w, r, errors.New("PostData CreatedAt field not set"), http.StatusBadRequest)
+		return ""
+	}
+
+	return fmt.Sprintf(
+		"/%d/%d/%d/%s",
+		pd.CreatedAt.Year(),
+		pd.CreatedAt.Month(),
+		pd.CreatedAt.Day(),
+		pd.Title,
+	)
+}
+
+func (s *server) storeBlogPost(w http.ResponseWriter, r *http.Request, blogPost *PostData) {
+	if err := s.DB.StoreBlogPost(blogPost); err != nil {
+		if err == glitch.ErrItemAlreadyExists {
+			s.respond(w, r, err, http.StatusConflict)
+			return
+		}
+		s.respond(w, r, err, http.StatusInternalServerError)
+		return
 	}
 }
