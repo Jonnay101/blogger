593fa4b0ad2c3c8685aad36704607937e9b59255
John Hughes <john.hughes101@gmail.com>
2020-04-12 11:58:18 +0100

    DatabaseKey will be index id

--- pkg/blog/model.go
+++ pkg/blog/model.go
@@ -1,7 +1,6 @@
 package blog
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/music-tribe/uuid"
@@ -9,27 +8,13 @@ import (
 
 // PostData holds all data and content from a blog postData
 type PostData struct {
-	UUID        uuid.UUID `json:"_id" bson:"id"`
+	DatabaseKey string    `json:"_id"`
+	UUID        uuid.UUID `json:"uuid" bson:"uuid"`
 	Author      string    `json:"author"`
 	Title       string    `json:"title"`
 	CreatedAt   time.Time `json:"created_at" bson:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at" bson:"updated_at"`
 	Content     string    `json:"content"`
 	Category    string    `json:"category"`
-	DatabaseKey string    `json:"database_key"`
 	Images      []string  `json:"images"` // a list of image IDs - images already uploaded
-}
-
-func (pd *PostData) setUUID() {
-	pd.UUID = uuid.New()
-}
-
-func (pd *PostData) setDateCreatedAt() {
-	pd.CreatedAt = time.Now()
-	pd.UpdatedAt = pd.CreatedAt
-}
-
-func (pd *PostData) setDatabaseKey() {
-	t := pd.CreatedAt
-	pd.DatabaseKey = fmt.Sprintf("/%v/%v/%v/%s", t.Year(), t.Month(), t.Day(), pd.Title)
 }
