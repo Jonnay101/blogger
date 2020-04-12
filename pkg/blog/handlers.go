4d4ab875ac99f956baae0f9d404b29c4721816c3
John Hughes <john.hughes101@gmail.com>
2020-04-12 01:20:13 +0100

    added db method in to handler

--- pkg/blog/handlers.go
+++ pkg/blog/handlers.go
@@ -7,10 +7,18 @@ import (
 // HandlerCreatePost stores a newly created blog post
 func (s *server) HandlerCreatePost() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var blogPost *PostData
-		if err := s.decodeRequestBody(w, r, blogPost); err != nil {
+
+		var blogPost PostData
+		if err := s.decodeRequestBody(w, r, &blogPost); err != nil {
 			s.respond(w, r, err, http.StatusBadRequest)
 			return
+		}
+
+		blogPost.setUUID()
+		blogPost.setDatabaseKey()
+
+		if err := s.DB.CreateBlogPost(&blogPost); err != nil {
+			s.respond(w, r, err, http.StatusInternalServerError)
 		}
 
 		s.respond(w, r, blogPost, http.StatusOK)
