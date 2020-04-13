7dbcc5125ba5586a66f42e7d7461b68d366abc56
John Hughes <john.hughes101@gmail.com>
2020-04-13 20:19:44 +0100

    routes updated

--- pkg/blog/routes.go
+++ pkg/blog/routes.go
@@ -1,17 +1,29 @@
 package blog
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func (s *server) setRoutes() {
 	s.Router = mux.NewRouter()
+
 	// handle creating new post
-	s.Router.Handle("/blog", adminOnly(s.HandlerCreatePost())).Methods("POST")
+	s.Router.Handle("/blog", adminOnly(s.HandlerCreatePost())).Methods(http.MethodPost)
+
 	// handle getting single post request
-	s.Router.Handle("/blog/{year}/{month}/{day}/{uuid}", s.HandlerGetPost()).Methods("GET")
+	s.Router.Handle("/blog/{year}/{month}/{day}/{uuid}", s.HandlerGetPost()).Methods(http.MethodGet)
+
 	// handle getting multiple posts from corresponding partial urls
-	s.Router.Handle("/blog/{year}/{month}/{day}/{uuid}", s.HandlerGetAllPosts()).Methods("GET")
-	s.Router.Handle("/blog/{year}/{month}/{day}", s.HandlerGetAllPosts()).Methods("GET")
-	s.Router.Handle("/blog/{year}/{month}", s.HandlerGetAllPosts()).Methods("GET")
-	s.Router.Handle("/blog/{year}", s.HandlerGetAllPosts()).Methods("GET")
-	s.Router.Handle("/blog", s.HandlerGetAllPosts()).Methods("GET")
+	s.Router.Handle("/blog/{year}/{month}/{day}/{uuid}", s.HandlerGetAllPosts()).Methods(http.MethodGet)
+	s.Router.Handle("/blog/{year}/{month}/{day}", s.HandlerGetAllPosts()).Methods(http.MethodGet)
+	s.Router.Handle("/blog/{year}/{month}", s.HandlerGetAllPosts()).Methods(http.MethodGet)
+	s.Router.Handle("/blog/{year}", s.HandlerGetAllPosts()).Methods(http.MethodGet)
+	s.Router.Handle("/blog", s.HandlerGetAllPosts()).Methods(http.MethodGet)
+
+	// handle removing blog posts
+	s.Router.Handle("/blog/{year}/{month}/{day}/{uuid}", s.HandlerDeletePost()).Methods(http.MethodDelete)
 }
+
+// TODO: setup dynamic routes properly with regex
