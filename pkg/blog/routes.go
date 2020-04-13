24b24974fa009a6862d580c314498c5e0dd284d5
John Hughes <john.hughes101@gmail.com>
2020-04-13 13:42:04 +0100

    new route

--- pkg/blog/routes.go
+++ pkg/blog/routes.go
@@ -4,5 +4,6 @@ import "github.com/gorilla/mux"
 
 func (s *server) setRoutes() {
 	s.Router = mux.NewRouter()
-	s.Router.Handle("/", adminOnly(s.HandlerCreatePost())).Methods("POST")
+	s.Router.Handle("/blog/", adminOnly(s.HandlerCreatePost())).Methods("POST")
+	s.Router.Handle("/blog/{year}/{month}/{day}/{uuid}", s.HandlerGetPost()).Methods("GET")
 }
