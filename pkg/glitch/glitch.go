c46e592b073c54d2d56a1c499db9f3c0dcc0862c
John Hughes <john.hughes101@gmail.com>
2020-04-13 13:42:18 +0100

    new not rfound error

--- pkg/glitch/glitch.go
+++ pkg/glitch/glitch.go
@@ -3,6 +3,7 @@ package glitch
 import "errors"
 
 var (
+	ErrRecordNotFound       = errors.New("database record not found")
 	ErrItemAlreadyExists    = errors.New("item already exists in this collection")
 	ErrUnknownDatabaseError = errors.New("unknown database error")
 )
