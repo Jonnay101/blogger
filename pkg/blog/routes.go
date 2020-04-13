3ba9317fbe125b2a4aea425a40f9ef4e684b00dd
John Hughes <john.hughes101@gmail.com>
2020-04-13 18:24:24 +0100

    cover partial urls for getting all blog posts

--- pkg/blog/routes.go
+++ pkg/blog/routes.go
@@ -4,6 +4,14 @@ import "github.com/gorilla/mux"
 
 func (s *server) setRoutes() {
 	s.Router = mux.NewRouter()
-	s.Router.Handle("/blog/", adminOnly(s.HandlerCreatePost())).Methods("POST")
+	// handle creating new post
+	s.Router.Handle("/blog", adminOnly(s.HandlerCreatePost())).Methods("POST")
+	// handle getting single post request
 	s.Router.Handle("/blog/{year}/{month}/{day}/{uuid}", s.HandlerGetPost()).Methods("GET")
+	// handle getting multiple posts from corresponding partial urls
+	s.Router.Handle("/blog/{year}/{month}/{day}/{uuid}", s.HandlerGetAllPosts()).Methods("GET")
+	s.Router.Handle("/blog/{year}/{month}/{day}", s.HandlerGetAllPosts()).Methods("GET")
+	s.Router.Handle("/blog/{year}/{month}", s.HandlerGetAllPosts()).Methods("GET")
+	s.Router.Handle("/blog/{year}", s.HandlerGetAllPosts()).Methods("GET")
+	s.Router.Handle("/blog", s.HandlerGetAllPosts()).Methods("GET")
 }
